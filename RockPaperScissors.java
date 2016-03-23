72757181eeace5f3585008354b08971cde66383d
sacs112cidalejandro <alexcid91@gmail.com>
2016-03-22 20:53:58 -0700

    Added files via upload

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,85 +1,41 @@
-import java.util.Scanner; 
-import java.util.Random; 
+import java.util.*;
+
+public class RockPaperScissors{
+	
+    public static void main(String[]args){
+    	
+    Scanner input = new Scanner(System.in);
+    Random randomNumber = new Random();
+    System.out.println("Want to play Rock, Paper, Scissors?");
+    System.out.print("\nEnter 0 for Scissors, 1 for Rock , or 2 for Paper: ");
+    int player = input.nextInt();
+    int computer = randomNumber.nextInt(3);
+    for (int round = 1; round < 4; round++) {
+		computer = 0 + (int) ( Math.random() * 10 );
 
 
-public class RockPaperScissors 
-{ 
-public static void main(String[]args) 
-
-{   //Input
-    String personPlay;
-    String computerPlay = ""; 
-    int computerInt;
-    String response; 
-
-    boolean status=true;
-    while (status)
-    {
-    Scanner scan = new Scanner(System.in); 
-    Random generator = new Random(); 
-
-    System.out.println("Want to play Rock, Paper, Scissors?\n");
-    System.out.println("Enter your move.");
-    System.out.println("R = Rock , P = Paper, and S = Scissors.");
-                      
-
-    System.out.println();
-
-  
-    computerInt = generator.nextInt(3)+1; 
-
-
-    //Conditional Statement 
-    if (computerInt == 1) 
-       computerPlay = "R"; 
-    else if (computerInt == 2) 
-       computerPlay = "P"; 
-    else if (computerInt == 3) 
-       computerPlay = "S"; 
-
-
-     
-    System.out.print("Enter your move:"); 
-    personPlay = scan.next();
-
-     
-    personPlay = personPlay.toUpperCase(); 
-
-    //Output
-    System.out.println("Computer play is: " + computerPlay); 
-
-
-    //Conditional Statement
-    if (personPlay.equals(computerPlay)) 
-       System.out.println("\nTie!"); 
-    else if (personPlay.equals("R")) 
-       if (computerPlay.equals("S")) 
-          System.out.println("\nYou Win!");
-    else if (computerPlay.equals("P")) 
-            System.out.println("\nYou Lose!"); 
-    else if (personPlay.equals("P")) 
-       if (computerPlay.equals("S")) 
-       System.out.println("\nYou Lose!!"); 
-    else if (computerPlay.equals("R")) 
-            System.out.println("\nYou Win!!"); 
-    else if (personPlay.equals("S")) 
-         if (computerPlay.equals("P")) 
-         System.out.println("\nYou Win!!"); 
-    else if (computerPlay.equals("\nR")) 
-            System.out.println("\nYou Lose!!"); 
-    else 
-         System.out.println("Invalid user input."); 
-    System.out.print("\nFor a new move press y or press j to shutdown:\n");
-    Scanner sc = new Scanner(System.in);
-    String input=sc.nextLine();
-    if (input.equalsIgnoreCase("J")) {
-        System.exit(0);
-    } else if (input.equalsIgnoreCase("Y\n")) {
-        status = true;
-    
-    
-
+    if (player == computer){
+        System.out.println("\nTie! Try Again!");
     }
-   }
-  }
+    else if(player == 0 && computer == 1){
+        System.out.println("\nComputer is Rock, You Lose!");
+    }
+    else if(player == 0 && computer == 2){
+        System.out.println("\nComputer is Paper, You Win!");
+    }
+    else if(player == 1 && computer == 0){
+            System.out.println("\nComputer is Scissor, You Win!");
+    }
+    else if(player == 1 && computer == 2){
+                System.out.println("\nComputer is Paper, You Lose!");
+    }
+    else if(player == 2 && computer == 0){
+                    System.out.println("\nComputer is Scissor, You Lose!");
+    }
+    else if(player == 2 && computer == 1){
+                        System.out.println("\nComputer is Rock, You Win!");
+    }
+
+}
+}
 }
